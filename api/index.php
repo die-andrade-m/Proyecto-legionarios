ea079af944b64dc61003c981f7b22071eac5be23
Google00111 <kayseer14@gmail.com>
2026-08-26 12:00:53 -0400

    Eliminar laravel/pail y redirigir caches de bootstrap para evitar error 500

--- api/index.php
+++ api/index.php
@@ -3,9 +3,27 @@
 // Configure Vercel serverless environment paths to writable /tmp
 putenv('VIEW_COMPILED_PATH=/tmp/views');
 putenv('APP_STORAGE=/tmp/storage');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
 
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 $_ENV['APP_STORAGE'] = '/tmp/storage';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+
+// Clean any stale cached bootstrap files in /bootstrap/cache
+if (file_exists(__DIR__ . '/../bootstrap/cache/packages.php')) {
+    @unlink(__DIR__ . '/../bootstrap/cache/packages.php');
+}
+if (file_exists(__DIR__ . '/../bootstrap/cache/services.php')) {
+    @unlink(__DIR__ . '/../bootstrap/cache/services.php');
+}
 
 // Create temporary directories in /tmp if missing
 $tmpDirs = [
